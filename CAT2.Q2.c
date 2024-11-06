e24992ff16100f429a7d9dbcf9459428e7c17703
mwangiDani <gitaridaniel36@gail.com>
2024-11-06 11:01:51 +0300

    update

---
+++ CAT2.Q2.c
@@ -0,0 +1,23 @@
+
+/*
+Name: MWANGI DANIEL GITARI
+DATE: 6/11/24
+REG NO : CT101/G/21346/24
+PROGRAM FOR CAT TWO QUESTION ONE STRUCTURE
+*/
+ 
+
+#include <stdio.h>
+int main (){
+	int i,j;
+	
+	int scores [2][4]={{65,92,84,72},{35,70,59,67}};
+		for (i=0;i<2;i++){
+		for (j=0;j<4;j++){
+			printf("scores[%d][%d]=%d\n",i,j,scores[i][j]);
+			
+		}
+	}
+	return 0;
+	
+}
